34db80782f3ddc647bdc4f43a8700f5af4ff344d
Luciano Liboni <luciano_liboni@hotmail.com>
2020-01-20 13:08:04 -0300

    Criando user service

--- src/test/java/com/wallet/repository/UserRepositoryTest.java
+++ src/test/java/com/wallet/repository/UserRepositoryTest.java
@@ -8,6 +8,7 @@ import org.junit.jupiter.api.Test;
 import org.junit.jupiter.api.TestInstance;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.boot.test.context.SpringBootTest;
+import org.springframework.test.context.ActiveProfiles;
 
 import java.util.Optional;
 
@@ -15,6 +16,7 @@ import static org.junit.jupiter.api.Assertions.*;
 
 @SpringBootTest
 @TestInstance(TestInstance.Lifecycle.PER_CLASS)
+@ActiveProfiles("test")
 public class UserRepositoryTest {
 
     private static final String EMAIL = "email@test.com";
---
+++ src/test/java/com/wallet/service/UserServiceTest.java
@@ -0,0 +1,42 @@
+package com.wallet.service;
+
+import com.wallet.entity.User;
+import com.wallet.repository.UserRepository;
+import org.junit.jupiter.api.BeforeAll;
+import org.junit.jupiter.api.Test;
+import org.junit.jupiter.api.TestInstance;
+import org.mockito.BDDMockito;
+import org.mockito.Mockito;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.boot.test.context.SpringBootTest;
+import org.springframework.boot.test.mock.mockito.MockBean;
+import org.springframework.test.context.ActiveProfiles;
+
+import java.util.Optional;
+
+import static org.junit.jupiter.api.Assertions.assertTrue;
+
+
+@SpringBootTest
+@TestInstance(TestInstance.Lifecycle.PER_CLASS)
+@ActiveProfiles("test")
+public class UserServiceTest {
+
+    @MockBean
+    UserRepository repository;
+
+    @Autowired
+    UserService service;
+
+    @BeforeAll
+    public void setUp(){
+        BDDMockito.given(repository.findByEmailEquals(Mockito.anyString())).willReturn(Optional.of(new User()));
+    }
+
+    @Test
+    public void testFindByEmail(){
+        Optional<User> user = service.findByEmail("email@teste.com");
+        assertTrue(user.isPresent());
+    }
+
+}
