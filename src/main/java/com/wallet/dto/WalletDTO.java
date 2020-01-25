ed09c184d4cd22feb41de2802f82c361e38727f0
Luciano Liboni <luciano_liboni@hotmail.com>
2020-01-25 13:30:19 -0300

    Criacao das entidades Wallet e WalletDTO

---
+++ src/main/java/com/wallet/dto/WalletDTO.java
@@ -0,0 +1,18 @@
+package com.wallet.dto;
+
+import lombok.Data;
+import org.hibernate.validator.constraints.Length;
+
+import javax.validation.constraints.NotNull;
+import java.math.BigDecimal;
+
+@Data
+public class WalletDTO {
+
+    private Long id;
+    @Length(min = 3)
+    @NotNull
+    private String name;
+    @NotNull
+    private BigDecimal value;
+}
