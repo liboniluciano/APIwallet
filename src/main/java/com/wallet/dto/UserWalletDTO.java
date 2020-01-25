7e0dbd3de9cccc02ad9fee4bba2c96aa88c74fa2
Luciano Liboni <luciano_liboni@hotmail.com>
2020-01-25 14:09:20 -0300

    Criado Entidade e DTO da UserWallet e seu repositorio

---
+++ src/main/java/com/wallet/dto/UserWalletDTO.java
@@ -0,0 +1,15 @@
+package com.wallet.dto;
+
+import lombok.Data;
+
+import javax.validation.constraints.NotNull;
+
+@Data
+public class UserWalletDTO {
+
+    private Long id;
+    @NotNull(message = "Informe o ID do usuário")
+    private Long users;
+    @NotNull(message = "Informe o ID da carteira ")
+    private Long wallet;
+}
